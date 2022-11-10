c29c3930e3fd8a6a9345397380d58c4ec3498e79
Austin <Harrenaustint@gmail.com>
2022-11-10 15:32:01 -0500

    push

---
+++ app.js
@@ -0,0 +1,11 @@
+
+
+
+function randomName(names)
+{
+    
+    var names = ['Austin', 'Ally', 'Kaitlin', 'Lauren', 'Spencer', 'Eric', 'Anthony'];
+    document.getElementById('answer').innerHTML = names[Math.floor(Math.random()* 7)];
+    
+}    
+
